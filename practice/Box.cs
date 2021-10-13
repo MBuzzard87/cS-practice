84a2de993511db98a1d0cdf4caff93eadd95a42a
Michael Buzzard <m.buzzard87@gmail.com>
2021-10-13 10:54:38 -0400

    Box update

--- practice/Box.cs
+++ practice/Box.cs
@@ -6,19 +6,24 @@ namespace practice
 {
     class Box
     {
-        private int length;
-        private int height;
-        private int width;
+     
         private int volume;
 
         public Box() { }
 
         public Box(int length, int height, int width)
         {
-            this.length = length;
-            this.height = height;
-            this.width = width;
-            volume = this.length * this.height * this.width;
+            if(length > 0 && height > 0 && width > 0)
+            {
+                Length = length;
+                Height = height;
+                Width = width;
+                volume = length * height * width;
+            } else
+            {
+                Console.WriteLine("enter a number greater than 1");
+            }
+            
         }
 
         public int Length { get; set; }
